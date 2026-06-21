fc2b0d157825456fa573353c996f36413b1fd352
Vamsi mohana reddy <mediboyinavamsimohanareddy@gmail.com>
2026-06-21 17:12:58 +0530

    Update backend base URL to Render live link

--- frontend/lib/utils/constants.dart
+++ frontend/lib/utils/constants.dart
@@ -1,11 +1,5 @@
-import 'package:flutter/foundation.dart';
-
 class AppConstants {
   static String get baseUrl {
-    if (kIsWeb) {
-      return 'http://localhost:5000/api'; // For Web (Chrome/Edge)
-    } else {
-      return 'http://10.0.2.2:5000/api'; // For Android emulator
-    }
+    return 'https://healthcare-management-system-6r3p.onrender.com/api';
   }
 }
